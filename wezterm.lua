9dd86ba6dce56892190cee4534ff86e6dc22dbf7
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-04-22 12:57:00 -0400

    remove the tab navigator using leader -2

--- wezterm.lua
+++ wezterm.lua
@@ -143,7 +143,6 @@ config.keys = {
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.tab_and_split_indices_are_zero_based = true
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
