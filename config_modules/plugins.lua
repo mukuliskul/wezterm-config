8a1a8b6ad8d8b7d2d6f92039c74bf17b1984c936
Mukul Sharma <mukul@mpac.io>
2026-02-12 11:37:07 -0500

    feat: add workplace switcher from ML

--- config_modules/plugins.lua
+++ config_modules/plugins.lua
@@ -2,7 +2,7 @@
 local wezterm = require("wezterm")
 
 return function(config)
-  -- local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-  -- smart_splits.apply_to_config(config)
+	local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+	workspace_switcher.zoxide_path = "/opt/homebrew/bin/zoxide"
+	workspace_switcher.apply_to_config(config)
 end
-
